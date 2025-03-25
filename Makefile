023dc344cb222d92189c47e2a831a98ab32888a8
Erlend Klakegg Bergheim <126939+klakegg@users.noreply.github.com>
2025-03-25 11:02:31 +0100

    Building schema_all.json

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 UBL_VERSION = 2.4
 UBL_URL = https://docs.oasis-open.org/ubl/os-UBL-$(UBL_VERSION)/mod/UBL-Entities-$(UBL_VERSION).gc
 
-build:
-	@ruby bin/build.rb
+build: target/schema_all.json
+
+clean:
+	@rm -rf target example/*.json
 
 convert:
 	@for f in $$(ls example/*.yaml); do \
@@ -12,5 +14,9 @@ convert:
 target/entities.gc:
 	@mkdir -p target
 	@wget -O target/entities.gc "$(UBL_URL)"
-target/entities.json: target/entities.gc
+
+target/entities.json: target/entities.gc bin/jsonify_entities.rb
 	@ruby bin/jsonify_entities.rb
+
+target/schema_all.json: target/entities.json bin/generate_all.rb $(shell ls src/definitions/*.json)
+	@ruby bin/generate_all.rb
---
+++ bin/generate_all.rb
@@ -0,0 +1,59 @@
+require 'json'
+
+src = JSON.load_file 'target/entities.json'
+
+def typeify(bies)
+  {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'required' => 
+      bies[0]['ModelName'].start_with?('UBL-CommonLibrary') ? [] : ["$schema"] + 
+      bies[1..]
+        .select { |bie| bie['Cardinality'].start_with? '1' }
+        .map { |bie| bie['ComponentName'] },
+    'properties' => 
+      (bies[0]['ModelName'].start_with?('UBL-CommonLibrary') ? {} : {"$schema" => {"$ref" => "#/definitions/common_schema"}}).merge(
+        bies[1..]
+        .map { |bie| [bie['ComponentName'], fieldify(bie)] }
+        .to_h,
+      )
+  }
+end
+
+def fieldify(bie)
+  ref = "#/definitions/#{bie['ComponentType'] == 'BBIE' ? "bbie_#{bie['RepresentationTerm'].gsub(' ', '').downcase}" : bie['RepresentationTerm'].gsub(' ', '')}"
+
+  if !bie['Cardinality'].end_with? '1' and bie['ComponentType'] == 'ASBIE'
+    {
+      'description' => bie['Definition'],
+      'oneOf' => [
+        {
+          '$ref' => ref,
+        },
+        {
+          'type' => 'array',
+          'items' => {
+            '$ref' => "#{ref}",
+          },
+        },
+      ],
+    }
+  else
+    {
+      'description' => bie['Definition'],
+      '$ref' => "#{ref}#{bie['Cardinality'].end_with?('1') ? '' : '_n'}",
+    }
+  end
+end
+
+schema = {
+  '$schema' => 'http://json-schema.org/draft-07/schema',
+  'definitions' => src.map { |id, bies| [id, typeify(bies)] }.to_h,
+}
+
+Dir['src/definitions/*.json'].sort.each do |file|
+  schema['definitions'][File.basename(file, '.json')] = JSON.load_file file
+end
+
+File.write 'target/schema_all_pretty.json', JSON.pretty_generate(schema)
+File.write 'target/schema_all.json', schema.to_json
