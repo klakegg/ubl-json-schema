61ccf5243f871051a9cbd4e8517d6f2fe547d067
Erlend Klakegg Bergheim <126939+klakegg@users.noreply.github.com>
2025-03-27 10:20:10 +0100

    Adding support for extensions and minor refactoring of code

--- src/ruby/generate_all.rb
+++ src/ruby/generate_all.rb
@@ -1,25 +1,44 @@
 require 'bundler/setup'
 require 'json'
 
-src = JSON.load_file 'target/entities.json'
-
 def typeify(bies)
+  required = []
+  properties = {}
+
+  # Add and require schema reference for ABIE not part of common library
+  if !bies[0]['ModelName'].start_with? 'UBL-CommonLibrary'
+    required.push '$schema'
+    properties.merge!({
+      '$schema' => {
+        '$ref': '#/definitions/common_schema',
+      },
+    })
+  end
+
+  # Add support for extensions in all ABIEs
+  properties.merge!({
+    'extensions' => {
+      '$ref': '#/definitions/extensions',
+    },
+  })
+  
+  # Gather required properties
+  required += bies[1..]
+    .select { |bie| bie['Cardinality'].start_with? '1' }
+    .map { |bie| bie['ComponentName'] }
+  # Gather properties
+  properties.merge! bies[1..]
+    .map { |bie| [bie['ComponentName'], fieldify(bie)] }
+    .to_h
+
+  # Return the type definition
   {
     'type' => 'object',
     'additionalProperties' => false,
-    'minProperties': 1,
-    'required' => 
-      bies[0]['ModelName'].start_with?('UBL-CommonLibrary') ? [] : ["$schema"] + 
-      bies[1..]
-        .select { |bie| bie['Cardinality'].start_with? '1' }
-        .map { |bie| bie['ComponentName'] },
-    'properties' => 
-      (bies[0]['ModelName'].start_with?('UBL-CommonLibrary') ? {} : {"$schema" => {"$ref" => "#/definitions/common_schema"}}).merge(
-        bies[1..]
-        .map { |bie| [bie['ComponentName'], fieldify(bie)] }
-        .to_h,
-      )
-}.compact
+    'minProperties' => required.size > 0 ? nil : 1,
+    'required' => required.size > 0 ? required : nil,
+    'properties' => properties,
+  }.compact
 end
 
 def fieldify(bie)
@@ -48,11 +67,16 @@ def fieldify(bie)
   end
 end
 
+# Load entities definition
+src = JSON.load_file 'target/entities.json'
+
+# Put together the schema
 schema = {
   '$schema' => 'http://json-schema.org/draft-07/schema',
   'definitions' => src.map { |id, bies| [id, typeify(bies)] }.to_h,
 }
 
+# Import handmade definitions
 Dir['src/definitions/*.json'].sort.each do |file|
   schema['definitions'][File.basename(file, '.json')] = JSON.load_file file
 end
