001ba7ce3b90adc5d427261696f4c55e23194884
Erlend Klakegg Bergheim <126939+klakegg@users.noreply.github.com>
2025-03-27 14:18:53 +0100

    Inserting definitions into typescript definitions

--- src/ruby/generate_ts.rb
+++ src/ruby/generate_ts.rb
@@ -6,6 +6,11 @@ src = JSON.load_file 'target/entities.json'
 
 # Create definitions
 src.each do |id, bies|
+
+  puts "/**"
+  puts " * #{bies[0]['Definition']} "
+  puts " */"
+
   puts "export interface #{id} {"
 
   if !bies[0]['ModelName'].start_with? 'UBL-CommonLibrary'
@@ -17,13 +22,17 @@ src.each do |id, bies|
   bies[1..].each do |bie|
     ref = "#{bie['ComponentType'] == 'BBIE' ? "bbie#{bie['RepresentationTerm'].gsub(' ', '')}" : bie['RepresentationTerm'].gsub(' ', '')}"
 
+    puts "  /**"
+    puts "   * #{bie['Definition']} "
+    puts "   */"
+
     if !bie['Cardinality'].end_with? '1' and bie['ComponentType'] == 'ASBIE'
       puts "  #{bie['ComponentName']}#{bie['Cardinality'].start_with?('1') ? '': '?'}: #{ref} | #{ref}[]"
     else
       puts "  #{bie['ComponentName']}#{bie['Cardinality'].start_with?('1') ? '': '?'}: #{ref}#{bie['Cardinality'].end_with?('1') ? '' : 'N'}"
     end
   end
-
+  
   puts "}"
   puts
 end
